a0eecc5e1b1e742d87662efa277097dd2fb255fc
carcheky <carcheky@gmail.com>
2016-06-19 15:22:20 +0200

    added Bundle Backup

---
+++ actions/Backup_All_Sites.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# sleep 0.2
+# export COLUMNS=20
+# source functions.sh
+# echo "----------> Example Action <------------------------------ "
+export COLUMNS=990
+source functions.sh
+BackupAllSites
