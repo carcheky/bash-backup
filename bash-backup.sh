b973aa01013c69700125e6347a5bb873a0a11cec
carcheky <carcheky@gmail.com>
2016-06-27 13:49:41 +0200

    removed dev files

--- bash-backup.sh
+++ bash-backup.sh
@@ -3,4 +3,11 @@ export COLUMNS=20
 source functions.sh
 clear
 
-getMenu
+
+if [[ $1 == backupall ]]; then
+   /bin/bash ./actions/Backup_All_Sites.sh
+  exit
+elif [[ 1 != backupall ]]; then
+  getMenu
+fi
+
