fe68be5a20ed5e64d2b295d89ef592ab79b52bcc
carcheky <carcheky@gmail.com>
2016-07-21 09:13:42 +0200

    added reinstall filey

---
+++ reinstall-backups-conf.sh
@@ -0,0 +1,1 @@
+cd; cp -fr bash-backup/backups-conf .; rm -fr bash-backup; git clone https://github.com/carcheky/bash-backup.git; cp -fr backups-conf/ bash-backup; cd bash-backup/; chmod +x bash-backup.sh; ./bash-backup.sh all
