a8d9a48b39136e566e701b00358078fab89fd744
carcheky <carcheky@users.noreply.github.com>
2019-02-01 09:33:22 +0100

    Update reinstall-backups-conf.sh

--- reinstall-backups-conf.sh
+++ reinstall-backups-conf.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 cd; cp -fr bash-backup/backups-conf .; rm -fr bash-backup; git clone https://github.com/carcheky/bash-backup.git; cp -fr backups-conf/ bash-backup; cd bash-backup/; chmod +x bash-backup.sh; ./bash-backup.sh all
